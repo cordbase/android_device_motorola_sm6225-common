d9baef16c3e1f3e7fd3b33ffb7bc1e1e474841dc
CordBase <144811064+cordbase@users.noreply.github.com>
2025-10-22 12:14:48 +0530

    common: Yaap Bringup

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -123,7 +123,7 @@ TARGET_MODULE_ALIASES += \
     wlan.ko:qca_cld3_wlan.ko
 
 # Partitions
--include vendor/lineage/config/BoardConfigReservedSize.mk
+-include vendor/yaap/config/BoardConfigReservedSize.mk
 
 BOARD_FLASH_BLOCK_SIZE := 262144 # (BOARD_KERNEL_PAGESIZE * 64)
 BOARD_USES_METADATA_PARTITION := true
